2e0dce2f064e908f1d9370cefa27eb3557e54efa
motogoturkiye-prog <motogoturkiye@gmail.com>
2026-09-15 01:37:20 +0300

    Update tv-program-data.js

--- tv/js/tv-program-data.js
+++ tv/js/tv-program-data.js
@@ -1,5 +1,12 @@
 const TV_PROGRAMS = [
   { title: "MotoGo TV", youtubeId: "KFERd5JfYSE" },
+  { title: "MotoGo Video 2", youtubeId: "krvZ6AowbhI" },
+  { title: "MotoGo Video 3", youtubeId: "C8I4odsI7r0" },
+  { title: "MotoGo Video 4", youtubeId: "gKIIRHsk6f4" },
+  { title: "MotoGo Video 5", youtubeId: "P545bpMAUzg" },
+  { title: "MotoGo Video 6", youtubeId: "enVonxtu6uc" },
+  { title: "MotoGo Video 7", youtubeId: "77YQi8DblE4" },
+  { title: "MotoGo Video 8", youtubeId: "XvLjnMTNzRs" },
 
   // Yeni video eklerken bu formatta ekle:
   // { title: "Video Başlığı", youtubeId: "VIDEO_ID_BURAYA" },
